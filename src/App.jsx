c0b2438ba21f336093f3b0f6b6b672518b0f9f2c
TheDarkLady <lakshmichaithanya13@gmail.com>
2024-08-22 23:45:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='App'>
+    <h1>My Todos </h1>
+    <div className='todo-wrapper'>
+      <div className='todo-input'>
+        <div className='todo-input-item'>
+          <label htmlFor="todo">Title</label>
+          <input type="text" id="todo" placeholder='Add Tasks'/>
+        </div>
+        <div className='todo-input-item'>
+          <label htmlFor="todo">Description</label>
+          <input type="text" id="todo" placeholder='Add Description'/>
+        </div>
+        <div className='todo-input-item'>
+          <button type='button'className='primary-btn'>Add Todo</button>
+        </div>
+      </div>
+    <div className='btn-area'>
+      <button className='secondaryBtn'>Todo</button>
+      <button className='secondaryBtn'>Completed</button>
+    </div>
+    <div className='todo-list-area'>
+      <div className='todo-list-item'>
+        <h3>Task 1</h3>
+        <p>Description</p>
+      </div>
+
+    </div>
+    </div>
+
+  </div>
+
+      
+    </>
+  )
+}
+
+export default App
